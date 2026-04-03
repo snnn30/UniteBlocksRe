5bb273f5d25e103766ffdae4c2afef42e4447206
snnn30 <keikam82a1@outlook.jp>
2026-04-04 01:29:15 +0900

    feature: BoardUniterの実装

---
+++ UniteBLocksRe.Test/Models/Services/BoardUniterTests.cs
@@ -0,0 +1,118 @@
+﻿using Godot;
+using Shouldly;
+using UniteBlocksRe.Models.Entities;
+using UniteBlocksRe.Models.Services;
+using UniteBlocksRe.Models.ValueObjects;
+
+namespace UniteBlocksRe.Test.Models.Services;
+
+public class BoardUniterTests
+{
+    [Fact(DisplayName = "基本的な合体テスト1")]
+    public void Test1()
+    {
+        var board = new BoardEntity();
+        var block = new BlockEntity(BlockColor.Red, new(2, 1));
+        var block2 = new BlockEntity(BlockColor.Red, new(2, 1));
+
+        board.TrySetBlock(new(0, 0), block);
+        board.TrySetBlock(new(0, 1), block2);
+        var result = BoardUniter.Unite(board);
+
+        result.HasUnited.ShouldBeTrue();
+        result.ChainCount.ShouldBe(1);
+        result.Steps[0].CreatedBlock.Size.ShouldBe(new Vector2I(2, 2));
+    }
+
+    [Fact(DisplayName = "基本的な合体テスト2")]
+    public void Test2()
+    {
+        var board = new BoardEntity();
+        var block = new BlockEntity(BlockColor.Red, new(1, 1));
+        var block2 = new BlockEntity(BlockColor.Red, new(1, 1));
+        var block3 = new BlockEntity(BlockColor.Red, new(1, 1));
+        var block4 = new BlockEntity(BlockColor.Red, new(1, 1));
+
+        board.TrySetBlock(new(0, 0), block);
+        board.TrySetBlock(new(0, 1), block2);
+        board.TrySetBlock(new(1, 0), block3);
+        board.TrySetBlock(new(1, 1), block4);
+        var result = BoardUniter.Unite(board);
+
+        result.HasUnited.ShouldBeTrue();
+        result.ChainCount.ShouldBe(1);
+        result.Steps[0].CreatedBlock.Size.ShouldBe(new Vector2I(2, 2));
+    }
+
+    [Fact(DisplayName = "合体できないパターンのテスト 色違い")]
+    public void Test3()
+    {
+        var board = new BoardEntity();
+        var block = new BlockEntity(BlockColor.Red, new(2, 1));
+        var block2 = new BlockEntity(BlockColor.Green, new(2, 1));
+
+        board.TrySetBlock(new(0, 0), block);
+        board.TrySetBlock(new(0, 1), block2);
+        var result = BoardUniter.Unite(board);
+
+        result.HasUnited.ShouldBeFalse();
+        result.ChainCount.ShouldBe(0);
+    }
+
+    [Fact(DisplayName = "合体できないパターンのテスト はみ出し")]
+    public void Test4()
+    {
+        var board = new BoardEntity();
+        var block = new BlockEntity(BlockColor.Red, new(1, 2));
+        var block2 = new BlockEntity(BlockColor.Red, new(1, 3));
+
+        board.TrySetBlock(new(0, 0), block);
+        board.TrySetBlock(new(1, 0), block2);
+        var result = BoardUniter.Unite(board);
+
+        result.HasUnited.ShouldBeFalse();
+        result.ChainCount.ShouldBe(0);
+    }
+
+    [Fact(DisplayName = "可能な限り大きくするテスト")]
+    public void Test5()
+    {
+        var board = new BoardEntity();
+        var block = new BlockEntity(BlockColor.Red, new(2, 1));
+        var block2 = new BlockEntity(BlockColor.Red, new(1, 2));
+        var block3 = new BlockEntity(BlockColor.Red, new(2, 1));
+
+        board.TrySetBlock(new(0, 0), block);
+        board.TrySetBlock(new(2, 0), block2);
+        board.TrySetBlock(new(0, 1), block3);
+        var result = BoardUniter.Unite(board);
+
+        result.HasUnited.ShouldBeTrue();
+        result.ChainCount.ShouldBe(1);
+        result.Steps[0].CreatedBlock.Size.ShouldBe(new Vector2I(3, 2));
+        result.Steps[0].Position.ShouldBe(new Vector2I(0, 0));
+    }
+
+    [Fact(DisplayName = "複数回の合体のテスト")]
+    public void Test6()
+    {
+        var board = new BoardEntity();
+        var block = new BlockEntity(BlockColor.Red, new(2, 1));
+        var block2 = new BlockEntity(BlockColor.Red, new(2, 1));
+        var block3 = new BlockEntity(BlockColor.Blue, new(2, 1));
+        var block4 = new BlockEntity(BlockColor.Blue, new(2, 2));
+
+        board.TrySetBlock(new(0, 0), block);
+        board.TrySetBlock(new(0, 1), block2);
+        board.TrySetBlock(new(5, 13), block3);
+        board.TrySetBlock(new(5, 11), block4);
+        var result = BoardUniter.Unite(board);
+
+        result.HasUnited.ShouldBeTrue();
+        result.ChainCount.ShouldBe(2);
+        result.Steps[0].CreatedBlock.Size.ShouldBe(new Vector2I(2, 2));
+        result.Steps[0].Position.ShouldBe(new Vector2I(0, 0));
+        result.Steps[1].CreatedBlock.Size.ShouldBe(new Vector2I(2, 3));
+        result.Steps[1].Position.ShouldBe(new Vector2I(5, 11));
+    }
+}
---
+++ UniteBlocksRe/src/Models/Services/BoardUniter.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using Godot;
+using UniteBlocksRe.Models.Entities;
+using UniteBlocksRe.Models.ValueObjects;
+
+namespace UniteBlocksRe.Models.Services;
+
+public static class BoardUniter
+{
+    public static UniteResult Unite(BoardEntity board)
+    {
+        var result = new List<UniteStep>();
+
+        for (var y = 0; y < BoardEntity.Size.Y; y++)
+        {
+            for (var x = 0; x < BoardEntity.Size.X; x++)
+            {
+                var pos = new Vector2I(x, y);
+                var (exists, block) = board.TryGetBlock(pos);
+                if (!exists)
+                {
+                    continue;
+                }
+
+                var origin = board.TryGetOrigin(block).Position;
+                if (origin != pos)
+                {
+                    continue;
+                }
+
+                var targetSize = CalculateLargestRectangle(board, block);
+                if (targetSize == block.Size || targetSize.X < 2 || targetSize.Y < 2)
+                {
+                    continue;
+                }
+
+                var step = PerformUnite(board, origin, targetSize, block.Color);
+                result.Add(step);
+            }
+        }
+
+        return new UniteResult(result);
+    }
+
+    private static UniteStep PerformUnite(
+        BoardEntity board,
+        Vector2I origin,
+        Vector2I shape,
+        BlockColor color
+    )
+    {
+        var targets = new HashSet<BlockEntity>();
+
+        for (var y = origin.Y; y < origin.Y + shape.Y; y++)
+        {
+            for (var x = origin.X; x < origin.X + shape.X; x++)
+            {
+                if (board.TryGetBlock(new(x, y)) is (true, var block))
+                {
+                    targets.Add(block);
+                }
+            }
+        }
+
+        foreach (var target in targets)
+        {
+            board.TryRemoveBlock(target);
+        }
+
+        var createdBlock = new BlockEntity(color, shape);
+        board.TrySetBlock(origin, createdBlock);
+
+        return new UniteStep(targets, createdBlock, origin);
+    }
+
+    private static Vector2I CalculateLargestRectangle(BoardEntity board, BlockEntity startBlock)
+    {
+        var largestSize = startBlock.Size;
+
+        var color = startBlock.Color;
+        var origin = board.TryGetOrigin(startBlock).Position;
+
+        for (var y = startBlock.Size.Y; origin.Y + y <= BoardEntity.Size.Y; y++)
+        {
+            for (var x = startBlock.Size.X; origin.X + x <= BoardEntity.Size.X; x++)
+            {
+                if (x * y <= largestSize.X * largestSize.Y)
+                {
+                    continue;
+                }
+
+                if (CanRangeFill(board, origin, new(x, y), color))
+                {
+                    largestSize.X = x;
+                    largestSize.Y = y;
+                }
+            }
+        }
+
+        return largestSize;
+    }
+
+    private static bool CanRangeFill(
+        BoardEntity board,
+        Vector2I origin,
+        Vector2I shape,
+        BlockColor color
+    )
+    {
+        for (var y = origin.Y; y < origin.Y + shape.Y; y++)
+        {
+            for (var x = origin.X; x < origin.X + shape.X; x++)
+            {
+                if (
+                    board.TryGetBlock(new Vector2I(x, y)) is not (true, var block)
+                    || block.Color != color
+                )
+                {
+                    return false;
+                }
+
+                var pos = board.TryGetOrigin(block).Position;
+
+                if (
+                    pos.X < origin.X
+                    || pos.X + block.Size.X > origin.X + shape.X
+                    || pos.Y < origin.Y
+                    || pos.Y + block.Size.Y > origin.Y + shape.Y
+                )
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}

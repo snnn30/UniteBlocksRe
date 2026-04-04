8d8892f61e4abd972c109f98184fc4fdd4ef0eaa
snnn30 <keikam82a1@outlook.jp>
2026-04-05 04:16:06 +0900

    feature: BlockQueueEntityの実装

---
+++ UniteBlocksRe/src/Models/Entities/BlockPairEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace UniteBlocksRe.Models.Entities;
+
+public class BlockPairEntity
+{
+    BlockEntity Parent { get; init; }
+    BlockEntity Child { get; init; }
+
+    public BlockPairEntity(BlockEntity parent, BlockEntity child)
+    {
+        Parent = parent;
+        Child = child;
+    }
+}
---
+++ UniteBlocksRe/src/Models/Entities/BlockQueueEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UniteBlocksRe.Models.ValueObjects;
+
+namespace UniteBlocksRe.Models.Entities;
+
+public class BlockQueueEntity
+{
+    public BlockPairEntity Next { get; private set; }
+    public BlockPairEntity NextNext { get; private set; }
+
+    public BlockQueueEntity()
+    {
+        Next = GeneratePair();
+        NextNext = GeneratePair();
+    }
+
+    public BlockPairEntity Dequeue()
+    {
+        var current = Next;
+        Next = NextNext;
+        NextNext = GeneratePair();
+        return current;
+    }
+
+    private static BlockPairEntity GeneratePair()
+    {
+        BlockEntity parent = new(GetRandomColor(), new(1, 1));
+        BlockEntity child = new(GetRandomColor(), new(1, 1));
+        return new(parent, child);
+    }
+
+    private static BlockColor GetRandomColor()
+    {
+        var values = Enum.GetValues<BlockColor>();
+        var random = new Random();
+        return values[random.Next(values.Length)];
+    }
+}
